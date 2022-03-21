f9c7dc15e90eaf2992a6cf323a665498383439cf
alexandresaints <alexandresaintss@gmail.com>
2022-03-21 17:31:53 -0300

    Coursify Finished

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -46,7 +46,6 @@ const navigation = useNavigation();
                 />
                 
             </ScrollView>
-            
             </Container>
             )}
             ListFooterComponent={() => <Footer/>}
